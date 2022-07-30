9d1ce28c0df71443bf579d8e9b646e0ca176da67
miere43 <x.miere@gmail.com>
2022-07-30 15:17:26 +0300

    Global Variables

--- CLox/vm.c
+++ CLox/vm.c
@@ -16,11 +16,13 @@ static void resetStack() {
 
 void initVM() {
 	resetStack();
+	initTable(&vm.globals);
 	initTable(&vm.strings);
 	vm.objects = NULL;
 }
 
 void freeVM() {
+	freeTable(&vm.globals);
 	freeTable(&vm.strings);
 	freeObjects();
 }
@@ -73,6 +75,7 @@ static InterpretResult run() {
 		double a = AS_NUMBER(pop()); \
 		push(valueType(a op b)); \
 	} while(false)
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 
 	for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -96,6 +99,32 @@ static InterpretResult run() {
 			case OP_NIL: push(NIL_VAL); break;
 			case OP_TRUE: push(BOOL_VAL(true)); break;
 			case OP_FALSE: push(BOOL_VAL(false)); break;
+			case OP_POP: pop(); break;
+
+			case OP_GET_GLOBAL: {
+				ObjString* name = READ_STRING();
+				Value value;
+				if (!tableGet(&vm.globals, name, &value)) {
+					runtimeError("Undefined variable '%s'.", name->chars);
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				push(value);
+			} break;
+
+			case OP_DEFINE_GLOBAL: {
+				ObjString* name = READ_STRING();
+				tableSet(&vm.globals, name, peek(0));
+				pop();
+			} break;
+
+			case OP_SET_GLOBAL: {
+				ObjString* name = READ_STRING();
+				if (tableSet(&vm.globals, name, peek(0))) {
+					tableDelete(&vm.globals, name);
+					runtimeError("Undefined variable '%s'.", name->chars);
+					return INTERPRET_RUNTIME_ERROR;
+				}
+			} break;
 
 			case OP_EQUAL: {
 				Value a = pop();
@@ -133,14 +162,18 @@ static InterpretResult run() {
 				push(NUMBER_VAL(-AS_NUMBER(pop())));
 			} break;
 
-			case OP_RETURN:
+			case OP_PRINT: {
 				printValue(pop());
 				printf("\n");
+			} break;
+
+			case OP_RETURN:
 				return INTERPRET_OK;
 		}
 	}
 
 #undef READ_BYTE
+#undef READ_STRING
 #undef READ_CONSTANT
 #undef BINARY_OP
 }
