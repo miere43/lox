8b0496e273db9c51558a591d7fb8c43f52a86378
miere43 <x.miere@gmail.com>
2022-08-07 23:27:32 +0300

    Optimization

--- CLox/value.h
+++ CLox/value.h
@@ -1,6 +1,54 @@
 #pragma once
 
 #include "common.h"
+#include <string.h>
+
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
+#ifdef NAN_BOXING
+
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+#define QNAN     ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL 1
+#define TAG_FALSE 2
+#define TAG_TRUE 3
+
+typedef uint64_t Value;
+
+static inline Value numToValue(double num) {
+	Value value;
+	memcpy(&value, &num, sizeof(num));
+	return value;
+}
+
+static inline double valueToNum(Value value) {
+	double num;
+	memcpy(&num, &value, sizeof(value));
+	return num;
+}
+
+#define NUMBER_VAL(value) numToValue(value)
+#define OBJ_VAL(obj) \
+	(Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+#define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define BOOL_VAL(value) ((value) ? TRUE_VAL : FALSE_VAL)
+
+#define AS_NUMBER(value) valueToNum(value)
+#define AS_BOOL(value) ((value) == TRUE_VAL)
+#define AS_OBJ(value) \
+	((Obj*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+
+#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
+#define IS_NIL(value) ((value) == NIL_VAL)
+#define IS_BOOL(value) (((value) | 1) == TRUE_VAL)
+#define IS_OBJ(value) \
+	(((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+
+#else
 
 typedef enum {
 	VAL_BOOL,
@@ -8,9 +56,6 @@ typedef enum {
 	VAL_NUMBER,
 	VAL_OBJ,
 } ValueType;
-
-typedef struct Obj Obj;
-typedef struct ObjString ObjString;
 
 typedef struct {
 	ValueType type;
@@ -34,6 +79,8 @@ typedef struct {
 #define IS_NIL(value)    ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 #define IS_OBJ(value)    ((value).type == VAL_OBJ)
+
+#endif
 
 typedef struct {
 	int capacity;
